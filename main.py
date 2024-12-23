800131e651d4bdaa8dd1c3bf362ff447cc9c390f
Kerem Enes Ersoy <73962938+keremenesersoy@users.noreply.github.com>
2024-12-23 20:51:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import os, random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,10 +12,23 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program get_duck_image_url fonksiyonunu çağırır'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
 @bot.command()
 async def mem(ctx):
-    with open('images\\mem1.jpg', 'rb') as f:
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
         picture = discord.File(f)
+ 
     await ctx.send(file=picture)
-
 bot.run("")
